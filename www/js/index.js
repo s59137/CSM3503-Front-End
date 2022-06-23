f0395fed46d2fa7cd2d1c67168a3b41e0c0017c1
s59137 <103542643+s59137@users.noreply.github.com>
2022-06-23 11:19:50 +0800

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,83 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+$(function(){
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    if (!localStorage.datacount || localStorage.datacount == null)
+        localStorage.datacount = 0;
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    var link1 = crossroads.addRoute('', function () {
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+        secondpage="<a href='secondpage.html?id="+employeeNumber+"'>"+email+"</a>";
+
+        var employeeNumber = $("#employeeNumber").val();
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        var officeCode = $("#officeCode").val();
+        var extension = $("#extension").val();
+        var email = $("#email").val();
+        var jobTitle = $("#jobTitle").val();
+        var reportsTo = $("#reportsTo").val();
+
+        mydata = {
+            employeeNumber:"",
+            firstname: "",
+            lastname: "",
+            officeCode: "",
+            extension: "",
+            email: "",
+            jobTitle:"",
+            reportsTo:""
+        };
+        mydata.employeeNum = employeeNumber;
+        mydata.first = firstName;
+        mydata.last = lastName;
+        mydata.officecode = officeCode;
+        mydata.ext = extension;
+        mydata.Email = email;
+        mydata.job = jobTitle;
+        mydata.report = reportsTo;
+
+
+        var i = localStorage.datacount
+        i++;
+        localStorage.setItem("data" + i, JSON.stringify(mydata));
+        localStorage.datacount = i;
+
+        $('#maintable tbody').html(htmlText);
+
+        $("#masterC").show();
+        
+        $.ajax({ 
+            type: "get", 
+            url: "https://kerbau.odaje.biz/getstaff.php", 
+            data: mydata, 
+            cache: false, 
+            success: function(mydata){ 
+             //instructions to execute when the ajax call is succeeds 
+             var myData = JSON.parse(mydata);
+             dataLength = localStorage.datacount;
+             htmlText = "";
+             if (dataLength > 0) {
+                 for (var i = 1; i <= dataLength; i++) {
+                     myData = localStorage.getItem("data" + i);
+                     myData = JSON.parse(mydata);
+                     htmlText = htmlText + "<tr><td>"+secondpage+"</td></tr>"
+                 }
+             } else {
+                 htmlText = htmlText + "<tr><td> no data found </td></tr>";
+             }
+             
+            },
+            error: function(){ 
+            console.log("ajax error!");
+            alert("Please contact admin!");
+            } 
+            }); 
+        
+    });
+
+    function parseHash(newHash,oldHash){
+        crossroads.parse(newHash);
+    }
+    hasher.initialized.add(parseHash);
+    hasher.changed.add(parseHash);
+    hasher.init();
+})
